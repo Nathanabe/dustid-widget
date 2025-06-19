fdd460ca981b59012b908f00bca4e196ce183bf9
Nathanabe <nathanieladehor4@gmail.com>
2025-06-19 01:24:34 -0700

    app.js

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/authRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/contacts', contactRoutes);
+
+export default app;
+
